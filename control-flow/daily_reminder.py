236de4951da4f1992030e3efb78ffaca83a10a48
Malika Edaif <edmalika957@gmail.com>
2024-12-22 20:12:45 +0100

    modify file

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,20 @@
+task = input("Enter your task:" )
+Priority = input("Priority (high/medium/low)")
+Time_bound =input("Is it time-bound?")
+
+
+match Priority:
+    case "high":
+         if Time_bound =="yes":
+            print("you have an important task Now, that requires immediate attention today!")
+         elif Time_bound =="No":
+           print("you have an important task Now")
+
+    case "medium" :
+        print("don't forget your project")
+
+    case "low" :
+        print("complet your project when you have time ")
+
+
+
