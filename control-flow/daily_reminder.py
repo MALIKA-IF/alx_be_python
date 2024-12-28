9729b65595340463f4093c48ccb6424e71ed6f82
Malika Edaif <edmalika957@gmail.com>
2024-12-28 23:10:04 +0100

    save some modification

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,33 +1,31 @@
 #ask the user for a single task
 task=input("Enter your task:" )
 #ask the user for its priority level
-priority=input("Priority (high/medium/low): ").lower()
+priority=input("Priority (high/medium/low): ")
 #ask the user for it is time-sensitive
-time_bound=input("Is it time-bound? (yes/no): ").lower()
+time_bound=input("Is it time-bound? (yes/no): ")
 
-#Finish project report' is a high priority task that requires immediate attention today!
+
 match priority:
     case "high":
          if time_bound =="yes":
-            reminder =f"Reminder:{task}is a ",priority,"priority task that requires immediate attention today!"
+            print("Reminder:",task,"is a ",priority,"priority task that requires immediate attention today!")
          else :
-           print(f"Note:{task}you have task Now,Consider completing it when you have free time")
+           print("Note:",task,"is a ",priority,"priority task,Consider completing it when you have free time")
 
-    case "medium" :
+    case "medium":
         if time_bound =="yes":
-              reminder =f"Reminder:'{task}'is a ",priority," priority task that requires immediate attention today!"
+              print("Reminder:",task,"is a ",priority," priority task that requires immediate attention today!")
         else :
-             print(f"Note:'{task}'you have task Now,Consider completing it when you have free time")
-
-
-    case "low" :
+             print("Note:",task,"is a ",priority," priority task,Consider completing it when you have free time")
+    case "low":
         if time_bound =="yes":
-             reminder =f"Reminder:{task}'is a ",priority," priority task that requires immediate attention today!"
-        else :
-             print(f"Note:{task}you have an important task Now,Consider completing it when you have free time")
+             print("Reminder:",task,"is a ",priority," priority task that requires immediate attention today!")
+        else:
+             print("Note:",task,"is a ",priority,"priority task, Consider completing it when you have free time")
     case _:
-         reminder ="invalid priority"
-print(reminder)
+         print("invalid priority")
+
 
 
 
