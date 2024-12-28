c450bf963f335960c8c1ae5f977ac9c72097615a
Malika Edaif <edmalika957@gmail.com>
2024-12-28 20:06:03 +0100

    add function

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,28 +1,28 @@
 #ask the user for a single task
-task = input("Enter your task:" )
+task = input("Enter your task:" ).str().lower()
 #ask the user for its priority level
-priority = input("Priority (high/medium/low)")
+priority = input("Priority (high/medium/low)").str().lower()
 #ask the user for it is time-sensitive
-time_bound =input("Is it time-bound? (yes, no)")
+time_bound =input("Is it time-bound? (yes, no)").str().lower()
 
 #Finish project report' is a high priority task that requires immediate attention today!
 match priority:
     case "high":
          if time_bound =="yes":
-            reminder ="'",task,"is a high priority task that requires immediate attention today!"
+            reminder ="'",task,"is a ",priority,"priority task that requires immediate attention today!"
          else :
             reminder ="you have task Now,Consider completing it when you have free time"
 
     case "medium" :
         if time_bound =="yes":
-              reminder ="'",task,"is a medium priority task that requires immediate attention today!"
+              reminder ="'",task,"is a ",priority," priority task that requires immediate attention today!"
         else :
               reminder ="you have task Now,Consider completing it when you have free time"
 
 
     case "low" :
         if time_bound =="yes":
-             reminder ="'",task,"is a low priority task that requires immediate attention today!"
+             reminder ="'",task,"is a ",priority," priority task that requires immediate attention today!"
         else :
               reminder ="you have an important task Now,Consider completing it when you have free time"
     case _:
