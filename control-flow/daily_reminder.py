6d9d81da1e9802855461bfabc49bf393c3e27bbc
Malika Edaif <edmalika957@gmail.com>
2024-12-28 20:42:17 +0100

    add function

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -9,25 +9,26 @@ time_bound=input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
          if time_bound =="yes":
-            reminder ="'",task,"is a ",priority,"priority task that requires immediate attention today!"
+            reminder =f"Reminder:{task}is a ",priority,"priority task that requires immediate attention today!"
          else :
-            reminder ="you have task Now,Consider completing it when you have free time"
+           print(f"Note:{task}you have task Now,Consider completing it when you have free time")
 
     case "medium" :
         if time_bound =="yes":
-              reminder ="'",task,"is a ",priority," priority task that requires immediate attention today!"
+              reminder =f"Reminder:'{task}'is a ",priority," priority task that requires immediate attention today!"
         else :
-              reminder ="you have task Now,Consider completing it when you have free time"
+             print(f"Note:'{task}'you have task Now,Consider completing it when you have free time")
 
 
     case "low" :
         if time_bound =="yes":
-             reminder ="'",task,"is a ",priority," priority task that requires immediate attention today!"
+             reminder =f"Reminder:{task}'is a ",priority," priority task that requires immediate attention today!"
         else :
-              reminder ="you have an important task Now,Consider completing it when you have free time"
+             print(f"Note:{task}you have an important task Now,Consider completing it when you have free time")
     case _:
          reminder ="invalid priority"
-print(reminder)      
+print(reminder)
+
 
 
 
