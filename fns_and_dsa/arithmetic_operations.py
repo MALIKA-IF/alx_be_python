972945650a4c044387255997d664d1f46c23c373
Malika Edaif <edmalika957@gmail.com>
2025-01-11 20:31:17 +0100

    define function perform_operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,14 @@
 #define function perform_operation
+
 def perform_operation(num1,num2,operation):
-    if operation == "add":
+    if operation=="add":
         result=num1+num2
-    elif operation =="subtract":
+    elif operation=="subtract":
         result=num1-num2
-    elif operation == "multiply":
+    elif operation=="multiply":
         result=num1*num2
     elif operation=="divide":
-        if num2 == 0:
+        if num2==0:
             print("you can not divide by 0")
         else:
             result=num1/num2
