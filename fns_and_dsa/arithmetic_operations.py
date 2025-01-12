2330300c715f0e25d6d1f042ccc8e39bd8bf42fc
Malika Edaif <edmalika957@gmail.com>
2025-01-12 16:27:04 +0100

    create file Shopping_list_manager.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,11 +7,10 @@ def perform_operation(num1, num2, operation):
         result=num1-num2
     elif operation=="multiply":
         result=num1*num2
-    elif operation=="divide":
-        try:
+    elif operation=="divide" & num2!=0:
             result=num1/num2
-        except num2==0:
-            result=0   
+    elif num2==0:
+        result=0   
     print(result)            
 
 
