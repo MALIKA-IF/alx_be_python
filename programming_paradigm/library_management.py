64ea1e8c01434ff1e9865268a1a4364665541759
Malika Edaif <edmalika957@gmail.com>
2025-01-22 21:33:22 +0100

    create file library_management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,55 @@
+class Book:
+  def __init__(self,title,author):
+     self.title=""
+     self.author=""
+     self._is_checked_out=False
+
+  def check_out(self):
+    if not self._is_checked_out:
+      self._is_checked_out=True
+      return True
+    return False  
+
+    pass
+  def return_book(self):
+    if self._is_checked_out:
+      self._is_checked_out=False
+      return False
+    return False
+  def is_available(self):
+    return not self._is_checked_out
+
+class Library:
+  def __init__(self):
+    self._books=[]
+
+  def add_book(self,book):
+    self._books.append(book)  
+
+  def check_out_book(self,title):
+    for book in self._books:
+      if book.title == title and book.is_available():
+        book.check_out()
+        print(f"book{title},checked our succefully")
+        return True
+    print(f"book{title}is not available")    
+
+  def return_book(self,title):
+    for book in self._books:
+      if book.title == title and not book.is_available():
+        book.return_book()
+        print(f"book{title}, is returned successfully")
+        return True
+    print(f"book{title}, not checked out")
+    return False
+  def list_available_books(self):
+    available_books = [book for book in self._books if book.is_available()] 
+    if available_books:
+      for book in available_books:
+        print(f"Title:{book.title} Author: {book.author}")
+      else:
+        print("No Books")  
+
+
+
+
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,12 +1,13 @@
 import unittest
-import SimpleCalculator 
+from Simple_calculator import SimpleCalculator
 
 class TestOperation(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(SimpleCalculator.add(5,7),12)
-        self.assertEqual(SimpleCalculator.subtract(5,7),12)
-        self.assertEqual(SimpleCalculator.multiply(5,7),35)
-        self.assertEqual(SimpleCalculator.divide(8,4),2)
+        result=SimpleCalculator.add(5,7)
+        self.assertEqual(result,12)
+       # self.assertEqual(subtract(5,7),12)
+       # self.assertEqual(multiply(5,7),35)
+       # self.assertEqual(divide(8,4),2)
 
 #run
 unittest.main()       
