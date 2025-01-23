9b02af65648b892973cc57fc24f85fb83770a805
Malika Edaif <edmalika957@gmail.com>
2025-01-23 21:23:27 +0100

    modify file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,21 +1,24 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
-        self.__account_balance=account_balance
+    def __init__(self, account_balance):
+        account_balance=0
+        self.account_balance=account_balance
 
-        
+#method deposit       
     def deposit(self ,amount):
-        return self.__account_balance+amount
-        pass
+        return self.account_balance+amount
+    
+#method withraw
     def withdraw(self, amount): 
-        if self.__account_balance !=0:
-          print("Withdrew: $",self.__account_balance-amount)
+        if self.account_balance !=0:
+          print("Withdrew: $",self.account_balance-amount)
           return True
-        elif self.__account_balance <= 0 & amount<self.__account_balance:
+        elif self.account_balance <= 0 & amount<self.account_balance:
           print("Insufficient funds")
           return False
-        pass
+        
+#method display balance        
     def display_balance(self):
-         print("Current Balance: $",self.__account_balance)
+         print("Current Balance: $",self.account_balance)
         
 
  
