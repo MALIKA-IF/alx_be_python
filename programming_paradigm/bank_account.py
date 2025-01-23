878fc5ac434c59e93d24cddf89e1848e68d9211f
Malika Edaif <edmalika957@gmail.com>
2025-01-23 22:28:27 +0100

    modify file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,9 +13,10 @@ class BankAccount:
           return True
           
         elif self.account_balance <= 0:
-      #    return "Insufficient funds"
+          return "Insufficient funds"
           return False
         elif amount < self.account_balance:
+          return "Insufficient funds"
           return False 
           
         
