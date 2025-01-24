065ac45e0640cc38418006dede8d5d0bdab7c27c
Malika Edaif <edmalika957@gmail.com>
2025-01-24 21:42:53 +0100

    modify file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,8 +2,9 @@ import unittest
 from Simple_calculator import SimpleCalculator
 
 class TestOperation(unittest.TestCase):
+
     def setUp(self):
-        self.calc=SimpleCalculator
+        self.calc=SimpleCalculator()
         
     def test_addition(self):
         self.assertEqual(self.calc.add(2,3),5)
